65bf65680aaeb04163290899af476207d73002ed
Lars Reed <lars@kalars.net>
2021-01-26 23:04:57 +0100

    After init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// eslint-disable-next-line no-unused-vars
+function AppAppApp() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +22,29 @@ function App() {
       </header>
     </div>
   );
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+        </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
