b98e9a81951ae1d7ca1c79231c8e6b8784ffdf58
Lars Reed <lars@kalars.net>
2021-01-26 23:41:22 +0100

    remove HelloWorld

--- frontend/src/components/todo/WelcomeComponent.jsx
+++ frontend/src/components/todo/WelcomeComponent.jsx
@@ -1,19 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import HelloWorldService from '../../api/todo/HelloWorldService.js';
-import AuthenticationService from './AuthenticationService.js';
 
 export default class WelcomeComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this);
-    this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this);
-    this.handleError = this.handleError.bind(this)
-    this.state = {
-      welcomeMessage: '',
-    };
-  }
-
   render() {
     return (
       <>
@@ -21,35 +9,7 @@ export default class WelcomeComponent extends Component {
         <div className='container'>
           Welcome, {this.props.match.params.name}! Your <Link to='/todos'>task list</Link>
         </div>
-        <div className='container'>
-          Click here for welcome message!
-          <button onClick={this.retrieveWelcomeMessage} className='btn btn-success'>
-            Click Me!
-          </button>
-        </div>
-        <div className='container'>{this.state.welcomeMessage}</div>
       </>
     );
-  }
-
-  retrieveWelcomeMessage() {
-    HelloWorldService.executeHelloWorldService(AuthenticationService.currentUser())
-      .then((response) => {
-        console.log(response);
-        return response;
-      })
-      .then((response) => this.handleSuccessfulResponse(response))
-      .catch(error => this.handleError(error));
-  }
-
-  handleSuccessfulResponse(response) {
-    this.setState({ welcomeMessage: response.data.message });
-  }
-
-  handleError(error) {
-    let errorMessage = '';
-    if (error.message) errorMessage += (error.message + ' ');
-    if (error.response && error.response.data) errorMessage += error.response.data.message;
-    this.setState({ welcomeMessage: errorMessage });
   }
 }
