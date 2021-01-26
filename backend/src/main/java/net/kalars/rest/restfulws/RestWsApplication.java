fe6a78e099751cd1a19356c3a349ad38e366259a
Lars Reed <lars@kalars.net>
2021-01-26 23:12:18 +0100

    From stat.spring.io

---
+++ backend/src/main/java/net/kalars/rest/restfulws/RestWsApplication.java
@@ -0,0 +1,13 @@
+package net.kalars.rest.restfulws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWsApplication.class, args);
+	}
+
+}
