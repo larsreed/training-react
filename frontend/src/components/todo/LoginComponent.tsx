919b2b5d6afcb5fa234446b5ed4dcdc09e1a0b13
Lars Reed <lars@kalars.net>
2021-02-19 23:10:42 +0100

    Updated login

--- frontend/src/components/todo/LoginComponent.tsx
+++ frontend/src/components/todo/LoginComponent.tsx
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import AuthenticationService from './AuthenticationService';
 import ShowError from './ErrorHandling';
 import { RouteComponentProps } from 'react-router-dom';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 
 interface LoginProps extends RouteComponentProps<any> {
   name: string;
@@ -21,6 +24,17 @@ export default class LoginComponent extends Component<LoginProps, LoginState> {
       password: '',
     };
     this.loginClicked = this.loginClicked.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.validateForm = this.validateForm.bind(this)
+  }
+
+  validateForm() {
+    return this.state.username.length > 0 && this.state.password.length > 0;
+  }
+
+  handleSubmit(event: any) {
+    event.preventDefault();
+    this.loginClicked();
   }
 
   loginClicked() {
@@ -45,31 +59,32 @@ export default class LoginComponent extends Component<LoginProps, LoginState> {
       <div className="maxWidth">
         <h1>Login</h1>
         <div className='container'>
-          <div className='container'>
-            <p>
-              Username:{' '}
-              <input
-                type='text'
-                name='username'
-                value={this.state.username}
-                onChange={(v) => this.setState({ username: v.target.value })}
-              />
-            </p>
-            <p>
-              Password:{' '}
-              <input
-                type='password'
-                name='password'
-                value={this.state.password}
-                onChange={(v) => this.setState({ password: v.target.value })}
-              />
-            </p>
-            <p>&nbsp;</p>
-            <button onClick={this.loginClicked} className='btn btn-success'>
-              Login
-            </button>
+
+        <Form onSubmit={this.handleSubmit}>
+        <Form.Group controlId="email">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            autoFocus
+            type="text"
+            autoComplete="username"
+            value={this.state.username}
+            onChange={(v) => this.setState({ username: v.target.value })}
+      />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            autoComplete="password"
+            value={this.state.password}
+            onChange={(v) => this.setState({ password: v.target.value })}
+      />
+        </Form.Group>
+        <Button block type="submit" disabled={!this.validateForm()}>
+          Login
+        </Button>
+      </Form>
           </div>
-        </div>
       </div>
     );
   }
